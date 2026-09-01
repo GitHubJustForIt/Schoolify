6445ce6f280415aca2900102e7e500179995bd1c
GitHubJustForIt <151542280+GitHubJustForIt@users.noreply.github.com>
2026-09-01 21:30:31 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1295,3 +1295,4 @@ Schoolify ist eine komplett clientseitige, kostenlose Schulorganisations‑App, 
 """
 AI_MAX_PROMPT_CHARS = 50
 MODEL_NAME = "llama-3.1-8b-instant"
+RESEND_API_KEY = "re_XZU6Y73b_9z22V3Mtvnu4uAgye9BtMAJK"
